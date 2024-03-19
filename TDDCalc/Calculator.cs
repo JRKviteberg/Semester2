1eeab69c7439ea984b64ee30e35e7809934ab69c
JRKviteberg <janrogerk@hotmail.com>
2024-03-19 09:57:33 +0100

    Uppdatering

---
+++ TDDCalc/Calculator.cs
@@ -0,0 +1,14 @@
+﻿namespace TDDCalc;
+
+public class Calculator
+{
+    public int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+    
+    public int Subtract(int num1, int num2)
+    {
+        return num1 - num2;
+    }
+}
